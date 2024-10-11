5214bc88145e24ccfb7ca24a570b8d6de07f99cc
1cgul <reluctt@gmail.com>
2024-10-10 22:12:04 -0400

    .

--- app/src/main/java/com/peakphysique/app/MainActivity.kt
+++ app/src/main/java/com/peakphysique/app/MainActivity.kt
@@ -7,9 +7,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.peakphysique.app.ui.theme.PeakPhysiqueTheme
-import com.peakphysique.app.view.LoginScreen
 import com.peakphysique.app.view.RegisterScreen
 
 class MainActivity : ComponentActivity() {
