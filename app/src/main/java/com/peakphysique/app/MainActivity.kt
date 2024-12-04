79c911340912cd44455ddc232a4c95910e2c172a
Omar Segura <seguo@farmingdale.edu>
2024-12-03 21:37:18 -0500

    added text to feed screen, only when empty

--- app/src/main/java/com/peakphysique/app/MainActivity.kt
+++ app/src/main/java/com/peakphysique/app/MainActivity.kt
@@ -7,14 +7,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.peakphysique.app.controller.Navigation
 import com.peakphysique.app.ui.theme.PeakPhysiqueTheme
-import com.peakphysique.app.view.RegisterScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,7 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             PeakPhysiqueTheme() {
                 Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize().background(
-                    Color(0xFFE0E0E0)
+                    Color(0xFFFFFFFF)
                 )) {
                     Navigation()
                 }
